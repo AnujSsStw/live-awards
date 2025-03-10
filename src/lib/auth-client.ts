fd55d28cc611ba1781c108617c78b645cbb3c173
Anuj Singh <anujzzztw@gmail.com>
2025-03-11 00:29:01 +0530

    base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   //   baseURL: "http://localhost:3000", // the base url of your auth server
-  baseURL: "https://presumably-crisp-sponge.ngrok-free.app",
+  // baseURL: "https://presumably-crisp-sponge.ngrok-free.app",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
